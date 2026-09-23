594007ccd46938a68c8ddf31eb460e0d5b882612
angemeli <angemeli019@gmail.com>
2026-09-23 20:03:49 +0100

    Exo 6 update

--- chapitre-02/exo6-le_filtre_qui_ne_s_active_jamais/SalleWorkspace/Salle/src/main.cpp
+++ chapitre-02/exo6-le_filtre_qui_ne_s_active_jamais/SalleWorkspace/Salle/src/main.cpp
@@ -9,7 +9,7 @@ int main() {
         fonction(); // L'appel n'est possible que si la macro est vraie
     #else
         cout << "Le programme compile sans MON_FILTRE defini";
-        fonction(); // Cette ligne reste commentée afin d'éviter une erreur de compilation
+        // fonction(); // Cette ligne reste commentée afin d'éviter une erreur de compilation
     #endif
     return 0;
 }
